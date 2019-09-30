b6ffc6718febfc2267bcb729d3c60f7b2c7fd809
user2 <askndr312>
2019-09-30 15:42:54 -0700

    Signin

--- app/src/main/java/com/example/jobsdigger/SignInScreen.java
+++ app/src/main/java/com/example/jobsdigger/SignInScreen.java
@@ -1,13 +1,117 @@
 package com.example.jobsdigger;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SignInScreen extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignInScreen extends AppCompatActivity
+{
+    EditText etemail,etpassword;
+    Button btnsignin,btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_screen);
+
+
+        etemail = findViewById(R.id.etemail);
+        etpassword = findViewById(R.id.etpassword);
+        btnsignin = findViewById(R.id.btnsignin);
+        btnSignup = findViewById(R.id.btnSignup);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplication(),UsertypeScreen.class);
+                startActivity(intent);
+            }
+        });
+
+
+        btnsignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataHandler();
+            }
+        });
+
+    }
+
+    private void dataHandler() {
+        String email = etemail.getText().toString();
+        String password = etpassword.getText().toString();
+        boolean isok = true;
+
+        if (isValidEmailAddress(email) == false) {
+            etemail.setError("Invalid Email");
+            isok = false;
+        }
+
+
+        if (password.length() < 8) {
+            etpassword.setError("password length eror");
+            isok = false;
+
+        }
+        if(isok)
+        {
+
+            signIn(email,password);
+        }
+
+
+
+    }
+
+
+    private boolean isValidEmailAddress(String email) {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+
+    }
+
+    private void signIn(String email,String pass)
+    {
+        FirebaseAuth auth=FirebaseAuth.getInstance();
+        auth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful())
+                {
+                    //todo go to, main screen(all task activity)
+
+
+                    Intent i =new Intent(getApplication(),JobslistScreen.class);
+                    startActivity(i);
+
+                }
+                else
+                {
+                    etemail.setError("email or password is wrong");
+
+
+
+                }
+
+
+            }
+        });
+
+
+
+
+
     }
 }
