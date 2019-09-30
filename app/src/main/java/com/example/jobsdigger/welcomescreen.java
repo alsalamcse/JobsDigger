f819c54b2261edee1b08f662413930cf8d727fb4
user2 <askndr312>
2019-09-30 15:23:15 -0700

    welcome

--- app/src/main/java/com/example/jobsdigger/welcomescreen.java
+++ app/src/main/java/com/example/jobsdigger/welcomescreen.java
@@ -1,5 +1,6 @@
 package com.example.jobsdigger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,27 @@ public class welcomescreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcomescreen);
+
+
+        Thread th=new Thread()
+        {
+            @Override
+            public void run() {
+                try {
+                    sleep(3*1000);
+                    Intent i=new Intent(getApplication(),SignInScreen.class);
+                    startActivity(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        };
+        th.start();
     }
 }
+
+
+
+
+
