37cc79d9b741e888873db1f04285f1ced93071fd
user2 <askndr312>
2019-09-30 15:57:11 -0700

    usertype

--- app/src/main/java/com/example/jobsdigger/UsertypeScreen.java
+++ app/src/main/java/com/example/jobsdigger/UsertypeScreen.java
@@ -1,13 +1,38 @@
 package com.example.jobsdigger;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UsertypeScreen extends AppCompatActivity {
+    Button btnCompany,btnJobSerarcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usertype_screen);
+
+        btnCompany=findViewById(R.id.btnCompany);
+        btnJobSerarcher=findViewById(R.id.btnJobSearcher);
+
+
+        btnCompany.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent i=new Intent(getApplication(),CompanySignUpScreen.class);
+                startActivity(i);
+            }
+        });
+
+        btnJobSerarcher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplication(),CustomerSignUpScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
