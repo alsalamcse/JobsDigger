8229b2775761f46dce8f6ea02773fe3793106fde
Owner <Askndr312@gmail.com>
2019-10-14 09:20:28 +0300

    FIX

--- app/src/main/java/com/example/jobsdigger/welcomescreen.java
+++ app/src/main/java/com/example/jobsdigger/welcomescreen.java
@@ -4,31 +4,51 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class welcomescreen extends AppCompatActivity {
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Thread th = new Thread() {
+            @Override
+            public void run() {
+                try {
+
+                    sleep(3 * 1000);
+
+
+                    FirebaseAuth auth = FirebaseAuth.getInstance();
+
+                    if (auth.getCurrentUser() == null || auth.getCurrentUser().getEmail() == null) {
+                        Intent i = new Intent(getApplication(), SignInScreen.class);
+                        startActivity(i);
+                        finish();
+                    } else {
+                        Intent i = new Intent(getApplication(), JobslistScreen.class);
+                        startActivity(i);
+                        finish();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        th.start();
+
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcomescreen);
-
-
-        Thread th=new Thread()
-        {
-            @Override
-            public void run() {
-                try {
-                    sleep(3*1000);
-                    Intent i=new Intent(getApplication(),HomeScreen.class);
-                    startActivity(i);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-
-        };
-        th.start();
     }
 }
+
 
 
 
