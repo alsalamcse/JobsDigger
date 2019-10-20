19cf1361d8ff3612e31af1ffbea45e0aefc85ff9
Owner <Askndr312@gmail.com>
2019-10-20 10:30:57 +0300

    AddJOBs

--- app/src/main/java/com/example/jobsdigger/AddJobScreen.java
+++ app/src/main/java/com/example/jobsdigger/AddJobScreen.java
@@ -1,10 +1,17 @@
 package com.example.jobsdigger;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import data.MyJob;
 
@@ -29,42 +36,72 @@ public class AddJobScreen extends AppCompatActivity {
             public void onClick(View view) {
                 dataHandler();
             }
-
-            private void dataHandler()
-            {
-                boolean isok=true;
-                String Subject=etSubject.getText().toString();
-                String place=etplace.getText().toString();
-                String Company=etCompany.getText().toString();
-                String TermsOfAcceptane=etTerms.getText().toString();
-
-                if (Subject.length()==0){
-                    etSubject.setError("Enter Subject");
-                    isok=false;
-                }
-                if (place.length()==0){
-                    etplace.setError("Enter Place");
-                    isok=false;
-                }
-                if (Company.length()==0){
-                    etCompany.setError("Enter Company");
-
-                }
-                if (TermsOfAcceptane.length()==0){
-                    etTerms.setError("Enter TermsOfAcceptane");
-                    isok=false;
-                }
-
-                if (isok)
-                {
-                    MyJob myJob=new MyJob();
-
-                }
-
-
-
-            }
         });
+    }
+
+    private void dataHandler() {
+        boolean isok = true;
+        String Subject = etSubject.getText().toString();
+        String place = etplace.getText().toString();
+        String Company = etCompany.getText().toString();
+        String TermsOfAcceptane = etTerms.getText().toString();
+
+        if (Subject.length() == 0) {
+            etSubject.setError("Enter Subject");
+            isok = false;
+        }
+
+        if (place.length() == 0) {
+            etplace.setError("Enter Place");
+            isok = false;
+        }
+
+        if (Company.length() == 0) {
+            etCompany.setError("Enter Company");
+
+        }
+
+        if (TermsOfAcceptane.length() == 0) {
+            etTerms.setError("Enter TermsOfAcceptane");
+            isok = false;
+        }
+
+
+        if (isok) {
+            MyJob myJob = new MyJob();
+            myJob.setSubject(Subject);
+            createMyJob(myJob);
+
+        }
+
 
     }
+
+    private void createMyJob(MyJob myJob) {
+        //.1
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        //.2-save in database
+        DatabaseReference reference = firebaseDatabase.getReference();//make a barameter ( with var)
+        String key = reference.child("Jobs").push().getKey();
+        reference.child("Jobs").child(key).setValue(myJob).addOnCompleteListener(AddJobScreen.this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(AddJobScreen.this, "Add Successful", Toast.LENGTH_SHORT).show();
+                    finish();
+                } else {
+                    Toast.makeText(AddJobScreen.this, "Add Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+                }
+            }
+
+        });
+    }
 }
+
+
+
+
+
+
+
