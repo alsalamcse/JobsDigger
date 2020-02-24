aaec4d58e996a8b6dfd5b4ad53d8e6e352b2f9c5
Owner <Askndr312@gmail.com>
2020-02-24 09:42:36 +0200

    customerdetails

---
+++ app/src/main/java/data/FirebaseUtils.java
@@ -0,0 +1,17 @@
+package data;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FirebaseUtils
+{
+    public static FirebaseAuth auth=FirebaseAuth.getInstance();
+    public static FirebaseDatabase database=FirebaseDatabase.getInstance();
+    public static DatabaseReference getReference()
+    {
+        String uid = auth.getUid();
+        return database.getReference().child("Jobs").child(uid);
+
+    }
+}
--- app/src/main/java/data/JobAdapter.java
+++ app/src/main/java/data/JobAdapter.java
@@ -2,9 +2,18 @@ package data;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.jobsdigger.R;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 
 public class JobAdapter extends ArrayAdapter<MyJob>
 {
@@ -12,4 +21,43 @@ public class JobAdapter extends ArrayAdapter<MyJob>
     public JobAdapter(@NonNull Context context) {
         super(context, R.layout.jobsitem);
     }
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        //building item view
+        View vitem= LayoutInflater.from(getContext()).inflate(R.layout.jobsitem,parent,false);
+        final TextView tvSubject=vitem.findViewById(R.id.tvSubject);
+        TextView tvCompany=vitem.findViewById(R.id.tvCompany);
+        TextView tvPlace=vitem.findViewById(R.id.tvPlace);
+        TextView tvTerms=vitem.findViewById(R.id.tvTerms);
+        Button btnApply=vitem.findViewById(R.id.btnApplyJob);
+
+        final MyJob myJob = getItem(position);//getting data source
+
+        //todo טיפול : באירוע מחיקה
+
+                    FirebaseUtils.getReference().child(myJob.getKey()).removeValue(new DatabaseReference.CompletionListener() {
+                        @Override
+                        public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                            if(databaseError==null)
+                            {
+                                Toast.makeText(getContext(), "deleted", Toast.LENGTH_SHORT).show();
+
+                            }
+                            else {
+                                Toast.makeText(getContext(), "not deleted"+databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
+
+
+        //connect item view to data source
+        tvSubject.setText(myJob.getSubject());
+        tvCompany.setText(myJob.getCompaney());
+        tvPlace.setText(myJob.getPlace());
+        tvTerms.setText(myJob.getTermsofAcceptanc());
+
+        return vitem;
+    }
+
 }
