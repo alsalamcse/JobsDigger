b47d187264320c93f1bb009aeeec5619423e7b67
moaad <rakaz.hammad12@gmail.com>
2020-03-03 08:54:37 +0200

    Merge branch 'master' of D:\jwadlastproject\JobsDigger with conflicts.

--- app/src/main/java/data/JobAdapter.java
+++ app/src/main/java/data/JobAdapter.java
@@ -9,8 +9,11 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.jobsdigger.R;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 
 public class JobAdapter extends ArrayAdapter<MyJob>
 {
@@ -28,15 +31,26 @@ public class JobAdapter extends ArrayAdapter<MyJob>
         TextView tvTerms=vitem.findViewById(R.id.tvTerms);
         Button btnApply=vitem.findViewById(R.id.btnApplyJob);
 
-        btnApply.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        final MyJob myJob = getItem(position);//getting data source
 
-            }
-        });
 
-        final MyJob myJob = getItem(position);
 
+        //todo טיפול : באירוע מחיקה
+
+                    FirebaseUtils.getReference().child(myJob.getKey()).removeValue(new DatabaseReference.CompletionListener() {
+                        @Override
+                        public void onComplete(@Nullable DatabaseError databaseError, @NonNull DatabaseReference databaseReference) {
+                            if(databaseError==null)
+                            {
+                                Toast.makeText(getContext(), "deleted", Toast.LENGTH_SHORT).show();
+
+                            }
+                            else {
+                                Toast.makeText(getContext(), "not deleted"+databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
 
 
         //connect item view to data source
